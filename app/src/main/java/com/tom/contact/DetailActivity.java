060b69c47e40fb837e2bc08a3837764b087431cf
Hank Tom <hanktom@gmail.com>
2015-07-31 13:27:11 +0800

    顯示原圖, 加入GestureDetector, Log

--- app/src/main/java/com/tom/contact/DetailActivity.java
+++ app/src/main/java/com/tom/contact/DetailActivity.java
@@ -1,17 +1,42 @@
 package com.tom.contact;
 
 import android.app.Activity;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.ImageView;
 
 
-public class DetailActivity extends Activity {
+public class DetailActivity extends Activity implements GestureDetector.OnGestureListener{
+
+    private ImageView image;
+    private GestureDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        long imageId = getIntent().getLongExtra("IMAGE_ID", -1);
+        int pos = getIntent().getIntExtra("POS", -1);
+        findViews();
+        image.setImageURI(
+                Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, imageId + ""));
+        detector = new GestureDetector(this, this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return detector.onTouchEvent(event);
+    }
+
+    private void findViews() {
+        image = (ImageView) findViewById(R.id.detail_image);
     }
 
     @Override
@@ -34,5 +59,40 @@ public class DetailActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        Log.d("GESTURE", "onDown");
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        Log.d("GESTURE", "onDown");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        Log.d("GESTURE", "onDown");
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        Log.d("GESTURE", "onDown");
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        Log.d("GESTURE", "");
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.d("GESTURE", "onFling");
+        return false;
     }
 }
--- app/src/main/java/com/tom/contact/ThumbActivity.java
+++ app/src/main/java/com/tom/contact/ThumbActivity.java
@@ -2,17 +2,20 @@ package com.tom.contact;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.SimpleCursorAdapter;
 
 
-public class ThumbActivity extends Activity {
+public class ThumbActivity extends Activity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +24,18 @@ public class ThumbActivity extends Activity {
         GridView grid = (GridView) findViewById(R.id.grid);
         ContentResolver cr = getContentResolver();
         Cursor c = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
+        while(c.moveToNext()){
+            int id = c.getInt(c.getColumnIndex(MediaStore.Images.Media._ID));
+            Log.d("CURSOR", id+"");
+        }
 //        Log.d("THUMB", c.getCount()+"");
         String[] from = {MediaStore.Images.Media.DATA, MediaStore.Images.Media.DISPLAY_NAME};
         int[] to = {R.id.thumb_image, R.id.thumb_text};
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.thumbnail, c, from, to, 1);
         grid.setAdapter(adapter);
-
+        grid.setOnItemClickListener(this);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,5 +57,15 @@ public class ThumbActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("ITEM", position+"/"+id);
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("IMAGE_ID", id);
+        intent.putExtra("POS", position);
+        startActivity(intent);
+
     }
 }
